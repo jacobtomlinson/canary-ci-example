f90edd5771b30c0a4d504b881b5cc9acbe36e21f
Jacob Tomlinson <jtomlinson@nvidia.com>
2022-03-18 11:53:25 +0000

    Tweak port configs

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ COPY app.py /app.py
 
 RUN pip install fastapi uvicorn[standard]
 
+EXPOSE 5000
+
 CMD python /app.py
--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def foo():
     return {"foo": "bar"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
